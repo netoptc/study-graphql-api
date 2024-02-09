41ae0af5a655eacc329b428354003139e0da1d9d
João Neto <joaoneto@Joaos-MacBook-Air.local>
2024-02-08 22:10:01 -0300

    add graphql

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,33 @@
 import express, { Request, Response } from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+	hello: () => {
+		return "Hello world!";
+	},
+};
 
 const app = express();
 
 app.get("/", (request: Request, response: Response) => {
 	return response.json({ message: "Hello World" });
 });
+
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema: schema,
+		rootValue: root,
+		graphiql: true,
+	})
+);
 
 export { app };
